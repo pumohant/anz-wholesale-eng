be0cc609592221765f9a6ebf84b753fae0d5c5ee
pmohanty <v-pmohanty@expediagroup.com>
2020-05-17 21:51:35 +0530

    New project fro ANZ wholesale

---
+++ src/main/java/com/anz/ANZWholesale/AnzWholesaleApplication.java
@@ -0,0 +1,13 @@
+package com.anz.ANZWholesale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnzWholesaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnzWholesaleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anz/ANZWholesale/AnzWholesaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anz.ANZWholesale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnzWholesaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
